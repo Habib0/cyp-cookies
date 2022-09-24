bd1ce3b77ce108a5187bc0473f3381648f0a50d2
Habib ullah <habibullah38@gmail.com>
2022-09-24 04:16:40 -0700

    first commit

---
+++ cypress/e2e/index.d.ts
@@ -0,0 +1,18 @@
+declare namespace Cypress {
+    interface Chainable {
+        loginUI()
+         /**
+     * Equivalent of `Cypress.Cookies.preserveOnce(name)`
+     * @example cy.preserveCookieOnce('connect.sid')
+     * @see https://github.com/bahmutov/cypress-v10-preserve-cookie
+     */
+    preserveCookieOnce(name: string): void
+
+    /**
+     * Equivalent of `Cypress.Cookies.preserveOnce(name, anotherName, ...)`
+     * @example cy.preserveCookieOnce('connect.sid', 'refresh_token')
+     * @see https://github.com/bahmutov/cypress-v10-preserve-cookie
+     */
+    preserveCookieOnce(...names: string[]): void
+    }
+}
