f6dacadc73b1c0c106c9d3e4a9bd8e636d9afc4f
hamza <hamza@imedhealth.us>
2023-06-09 11:17:31 +0500

    new brach create

--- cypress/support/commands.ts
+++
@@ -1,8 +0,0 @@
-Cypress.Commands.add("loginUI",()=>{
-    cy.visit('/')
-    cy.get('[name="userName"]').type('habibwebdev')
-    cy.get('[name="password"]').type('1')
-    cy.get('[aria-label="Sign in"]').click()
-    cy.wait(5000)
-    cy.get('[class="pull-right"]').should('be.visible')
-})
